072e654866fb0d885e0b97ea41af2a85c8061945
JuanjoseAR <jarango@unimagdalena.edu.co>
2025-08-07 21:31:23 -0500

    mejoras

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-MODELO_GPT = os.getenv("MODELO_GPT")
+try:
+    from env import (
+        DB_HOST_env, DB_NAME_env, DB_PASWORD_env, DB_USER_env, MODELO_GPT_env
+    )
+except ImportError:
+    DB_HOST_env = DB_NAME_env = DB_PASWORD_env = DB_USER_env = MODELO_GPT_env = None
+
+DB_NAME = os.getenv("DB_NAME", DB_NAME_env)
+DB_USER = os.getenv("DB_USER", DB_USER_env)
+DB_PASWORD = os.getenv("DB_PASSWORD", DB_PASWORD_env)
+DB_HOST = os.getenv("DB_HOST", DB_HOST_env)
+MODELO_GPT = os.getenv("MODELO_GPT", MODELO_GPT_env)
