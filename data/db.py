ffb86020e670f8fd1b84527da93394031b8a946c
JuanjoseAR <jarango@unimagdalena.edu.co>
2025-08-07 19:37:35 -0500

    MEJORAS

--- data/db.py
+++ data/db.py
@@ -1,13 +1,13 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from config import DB_NAME, DB_PASWORD, DB_USER
+from config import DB_NAME, DB_PASWORD, DB_USER, DB_HOST
 
 def get_connection():
     return psycopg2.connect(
         dbname=DB_NAME,
         user=DB_USER,
         password=DB_PASWORD,
-        host='localhost',
+        host=DB_HOST,
         port='5432',
         cursor_factory=RealDictCursor
     )
