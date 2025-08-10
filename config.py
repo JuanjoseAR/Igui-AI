fd7394e39a02713aaeba460333e923522a1e989f
JuanjoseAR <jarango@unimagdalena.edu.co>
2025-08-10 17:12:35 -0500

    mejoras

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ except ImportError:
 DB_NAME = os.getenv("DB_NAME", DB_NAME_env)
 DB_USER = os.getenv("DB_USER", DB_USER_env)
 DB_PASWORD = os.getenv("DB_PASSWORD", DB_PASWORD_env)
-DB_HOST = os.getenv("DB_HOST", DB_HOST_env)
+DB_HOST = os.getenv("DB_HOST", DB_HOST_env) 
 MODELO_GPT = os.getenv("MODELO_GPT", MODELO_GPT_env)
